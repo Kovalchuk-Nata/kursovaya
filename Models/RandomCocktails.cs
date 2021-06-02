6cabd5c40d83e6ea698972f8d94f90e6637d6ce4
Kovalchuk Nata <76482616+Kovalchuk-Nata@users.noreply.github.com>
2021-06-02 10:24:41 +0300

    Add files via upload

---
+++ Models/RandomCocktails.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kursovaya.Models
+{
+    public class ExtendedIngredientt
+    {
+        public string Original { get; set; }
+    }
+
+    public class Recipee
+    {
+        public List<ExtendedIngredientt> ExtendedIngredients { get; set; }
+        public string Title { get; set; }
+        public int ReadyInMinutes { get; set; }
+        public int Servings { get; set; }
+        public string Image { get; set; }
+        public string Instructions { get; set; }
+    }
+
+    public class RandomCocktails
+    {
+        public List<Recipe> Recipes { get; set; }
+    }
+}
