07bb30b028ca0103163c25571eb7757648203b56
Kovalchuk Nata <76482616+Kovalchuk-Nata@users.noreply.github.com>
2021-06-02 07:00:50 +0300

    Update Const.cs

--- Const.cs
+++ Const.cs
@@ -8,10 +8,9 @@ namespace kursovaya
     public class Const
     {
         public static string adress = "https://api.spoonacular.com";
-        public static string apiKey = "1686191358ed4208988e2f8fd63fd3ba";
+        public static string apiKey = "78012c77514e4758b4c2429edde46394"; //"1686191358ed4208988e2f8fd63fd3ba";
         public static string a_key = "AKIATKZNSKLMYVY6O22N"; //access key for dinamoDB
-        public static string s_key = "I3MDRZx7gqENpQTGq3PivvbLyge6m5s9Kwg9cG9K"; //secret key
+        public static string s_key = "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"; //secret key for dinamoDB
         public static string tableName = "recipes"; 
-
     }
 }
